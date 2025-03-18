a1ee15b3d6056b337d492201ace4438d23ac3de0
Samudra-Ekanayake <samudra2724@gmail.com>
2025-03-18 18:40:48 +0100

    completato la show

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -1,21 +1,24 @@
 @extends ('layouts.app')
 
 @section('content')
-    <div class="container-fluid mt-5">
+
+<h1 class="text-center mt-4"> Lista dei progetti</h1>
+
+    <div class="container-fluid mt-3">
         <div class="row d-flex justify-content-around m-1 ">
             @foreach ($project as $progetti)
                 <div class="card m-3" style="width: 25rem;">
-                    <img src="https://image.discovery.indazn.com/ca/v2/ca/image?id=ec9e0c88-2609-4083-934b-3142632043bd&quality=70"
+                    <img src="{{ $progetti->cover_image }}"
                         class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                            card's content.</p>
+                        <h5 class="card-title">{{$progetti->name}}</h5>
+                        <p class="card-text">{{$progetti->description}}</p>
+                        <p class="card-text">{{$progetti->creation_date}}</p>
                         <div class="container">
                             <div class="row">
                                 <div class="col-4">
                                     <a href="{{ route('project.show', $progetti->id) }}"
-                                        class="btn btn-primary w-100">Mostra</a>
+                                        class="btn btn-primary w-100">Dettagli</a>
                                 </div>
                                 <div class="col-4">
                                     <a href="#" class="btn btn-warning w-100">Modifica</a>
--- resources/views/project/show.blade.php
+++ resources/views/project/show.blade.php
@@ -1,1 +1,17 @@
 @extends ('layouts.app')
+
+@section('content')
+    <div class="container mt-5">
+
+        <h1 class="text-cent mb-3">{{ $project['name'] }}</h1>
+        <div class="card mb-3">
+            <img src="{{ $project['cover_image'] }}" class="card-img-top" alt="...">
+            <div class="card-body">
+                {{-- <h5 class="card-title">{{$project['name']}}</h5> --}}
+                <p>{{ $project['description'] }}</p>
+                <p class="card-text">{{ $project['creation_date'] }}</p>
+            </div>
+        </div>
+
+    </div>
+@endsection
